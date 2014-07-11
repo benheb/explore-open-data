74beb17e4a0bd282d82e8dc7761a9b78cec1dc43
chelm <christopher.helm@gmail.com>
2014-07-11 10:23:41 -0600

    tring to add points

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,7 +1,8 @@
 //MAP! 
 var map;
-require(["esri/map", "esri/layers/ArcGISTiledMapServiceLayer", 
-  "esri/layers/FeatureLayer", "dojo/domReady!"], function(Map) { 
+require(["esri/map", "dojo/request", "esri/geometry/Circle", "esri/symbols/SimpleFillSymbol", 
+  "esri/graphic", "esri/layers/GraphicsLayer", "esri/geometry/Point", "esri/layers/ArcGISTiledMapServiceLayer", 
+  "esri/layers/FeatureLayer", "dojo/domReady!"], function(Map, request, Circle, SimpleFillSymbol, Graphic, GraphicsLayer, Point) { 
 
   esriConfig.defaults.map.basemaps.dotted = {
     baseMapLayers: [
@@ -15,4 +16,50 @@ require(["esri/map", "esri/layers/ArcGISTiledMapServiceLayer",
     zoom: 3,
     basemap: "dotted"
   });
+  
+  var symbol = new SimpleFillSymbol().setColor(null).outline.setColor("blue");
+  var gl = new GraphicsLayer({ id: "circles" });
+  map.addLayer(gl);
+
+  function add(f){
+    console.log(f);
+    var point = new Point([-117.15,32.71]);
+    var circle = new Circle(point, {
+      radius: 100
+    });
+    var graphic = new Graphic(circle, symbol);
+    gl.add(graphic);
+  }
+
+  request("explore.json").then(function(data){
+    // do something with handled data
+    var sites = JSON.parse(data).sites, feature;
+
+    sites.forEach(function(site,i){
+      feature = {
+        geometry: {
+          x: site.default_extent.xmin + (site.default_extent.xmax - site.default_extent.xmin),
+          y: site.default_extent.ymin + (site.default_extent.ymax - site.default_extent.ymin),
+          type: 'point',
+          spatialReference: {
+            latestWkid: 3857,
+            wkid: 102100
+          }
+        },
+        attributes: {
+          title: site.title,
+          url: site.url,
+          created_at: site.created_at,
+          updated_at: site.updated_at,
+          public: site.public,
+          groups_count: site.groups_count,
+          datasets_count: site.datasets_count
+        }
+      };
+
+      add(feature);
+
+    });   
+  });
+   
 });
